d89b3ac1c3f7614174d9069a6ae56475d62a0f50
Evandro <evandro@live.jp>
2019-11-05 13:12:02 -0300

    add novas aulas

---
+++ 24_Objeto.js
@@ -0,0 +1,11 @@
+const prod1 = {}
+prod1.nome = 'Celular Ultra Mega'
+prod1.preco = 4999.90
+prod1['Desconto legal'] = 0.40 //Espaço nos atributos, EVITAR..
+console.log(prod1)
+
+const prod2 = {
+    nome: 'Camisa polo',
+    preco: 79.90
+}
+console.log(prod2)
