3daa40ae8ffd2e232562ca99ed823810dc9029cc
Evandro Pereira Dias <ev047953@intelbras.com.br>
2021-06-07 12:50:52 -0300

    Add novas aulas

---
+++ segundavez/Seção 3 - Fundamentos/S3_16-Config_projeto.js
@@ -0,0 +1,1 @@
+//Somente explicação das criações de pasta e arquivos
---
+++ segundavez/Seção 3 - Fundamentos/S3_17-Org_basica_cod_JS.js
@@ -0,0 +1,10 @@
+console.log("linha de código");
+
+{
+    /* Agrupar grupo de códigos {} */
+    //bloco de código, função, classe, objeto, controle, repetição, teste.
+    {
+        console.log("Olá")
+        console.log("Mundo!")
+    }
+}
