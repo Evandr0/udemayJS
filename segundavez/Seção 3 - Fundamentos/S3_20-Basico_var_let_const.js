c568893a6373ba24f448c32055bf45127c3a5cc2
Evandro Pereira Dias <ev047953@intelbras.com.br>
2021-06-11 18:36:09 -0300

    nova aula S3-20

---
+++ segundavez/Seção 3 - Fundamentos/S3_20-Basico_var_let_const.js
@@ -0,0 +1,16 @@
+var a = 3; //variavel
+let b = 4;
+
+var a = 30;
+
+b = 40;
+
+console.log(a, b)
+
+a = 300;
+b = 400;
+
+console.log("Varialvel A =", a,"\nVarialvel B =", b)
+
+const c = 5;
+console.log("constante =", c)
