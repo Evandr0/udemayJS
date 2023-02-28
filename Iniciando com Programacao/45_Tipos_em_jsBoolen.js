5ebfedd5659086b1c81cff0611f3ef352aba4d0f
ev047953 <evandro.dias@intelbras.com.br>
2023-02-28 18:05:29 -0300

    up aula 45_Tipos_em_jsBoolen.js

---
+++ Iniciando com Programacao/45_Tipos_em_jsBoolen.js
@@ -0,0 +1,13 @@
+//45 tipos em JavaScript: Boolean.
+
+let isAtivo = false;
+console.log(isAtivo);
+
+isAtivo = true;
+console.log(isAtivo)
+
+isAtivo = 1 ; //verdadeiro
+console.log(!!isAtivo); //verifica o boleano do numero colocando !! na frente.
+
+
+console.log('' || '123'); //imprime o primeiro valor verdadeiro
